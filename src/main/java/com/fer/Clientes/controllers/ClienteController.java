7d7a4396083da7a936b0673668fd50a42ff6ebce
27Fer12 <fr9780547@gmail.com>
2025-05-07 07:12:26 -0600

    Act6-Crando Metodo PUT en el cintrola

--- src/main/java/com/fer/Clientes/controllers/ClienteController.java
+++ src/main/java/com/fer/Clientes/controllers/ClienteController.java
@@ -3,8 +3,11 @@ package com.fer.Clientes.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,15 +22,29 @@ public class ClienteController {
     @Autowired
     private ClienteRepository clienteRepository;
 
-    // Método GET para obtener todos los clientes
+    // Obtener todos los clientes
     @GetMapping("/traerClientes")
     public List<Cliente> traerClientes() {
         return clienteRepository.findAll();
     }
 
-    // Método POST para insertar un nuevo cliente
+    // Insertar un nuevo cliente
     @PostMapping("/insertarCliente")
     public Cliente insertarCliente(@RequestBody Cliente cliente) {
         return clienteRepository.save(cliente);
+    }
+
+    // Editar un cliente existente
+    @PutMapping("/editar-Cliente/{id}")
+    public ResponseEntity<Cliente> actualizarCliente(@PathVariable Long id, @RequestBody Cliente cliente) {
+        return clienteRepository.findById(id).map(clienteExistente -> {
+            clienteExistente.setNombre(cliente.getNombre());
+            clienteExistente.setApellidos(cliente.getApellidos());
+            clienteExistente.setDni(cliente.getDni());
+            clienteExistente.setTelefono(cliente.getTelefono());
+            clienteExistente.setEmail(cliente.getEmail());
+            Cliente clienteActualizado = clienteRepository.save(clienteExistente);
+            return ResponseEntity.ok(clienteActualizado);
+        }).orElse(ResponseEntity.notFound().build());
     }
 }
