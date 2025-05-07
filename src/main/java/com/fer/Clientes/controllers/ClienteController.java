403d6c4b163c1d06e3c586921aac15d1f03b1f01
27Fer12 <fr9780547@gmail.com>
2025-05-07 01:43:00 -0600

    Act5-Crando el metodo Post

--- src/main/java/com/fer/Clientes/controllers/ClienteController.java
+++ src/main/java/com/fer/Clientes/controllers/ClienteController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +19,15 @@ public class ClienteController {
     @Autowired
     private ClienteRepository clienteRepository;
 
+    // Método GET para obtener todos los clientes
     @GetMapping("/traerClientes")
     public List<Cliente> traerClientes() {
         return clienteRepository.findAll();
+    }
+
+    // Método POST para insertar un nuevo cliente
+    @PostMapping("/insertarCliente")
+    public Cliente insertarCliente(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
     }
 }
