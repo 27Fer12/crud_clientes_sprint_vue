5b5711b5a0d6ef2aabe877e2b6ef3fac0cacfab2
27Fer12 <fr9780547@gmail.com>
2025-05-09 04:42:46 -0600

    Act12 Creando un metodo GET para traer la informacion del cliente

--- clientes/front_clientes/src/App.vue
+++ clientes/front_clientes/src/App.vue
@@ -1,13 +1,22 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
 
+const clientes = ref([])
+
+const cargarClientes = async () => {
+  const response = await axios.get('http://localhost:8080/clientes/traerClientes')
+  clientes.value = response.data
+  console.log(clientes.value)
+}
+
+onMounted(cargarClientes)
 </script>
 
 <template>
   <div class="container">
-    <button class="btn btn-primary">boton </button>
-    </div>
-  
- 
+    <button class="btn btn-primary">Botón</button>
+  </div>
 </template>
 
 <style scoped>
